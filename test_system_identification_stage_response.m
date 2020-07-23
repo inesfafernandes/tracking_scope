b5416e5c264d4d7a7514923c60e688f78af08b41
Inês Fernandes <inesfafern@gmail.com>
2020-07-23 17:44:12 +0100

    tunning PID controller

--- test_system_identification_stage_response.m
+++ test_system_identification_stage_response.m
@@ -1,15 +1,15 @@
 %% Reads and writes a signal, but not in a continous form
 
 dq = daq("ni"); %create data acquisition
-dq.Rate = 700; %set the generation scan rate; rate cant be the same as K (check system_identification_fc)
+dq.Rate = 10000; %set the generation scan rate; rate cant be the same as K (check system_identification_fc)
 addoutput(dq, "Dev1", "ao1", "Voltage");% adds analog output channel 
 addinput(dq, "Dev1", "ai1", "Voltage");% adds analog input channel 
 K=350;
 
 lim=10;
 
-input=zeros(5000,1);
-input(5001:10000,1)=1;
+input=zeros(50000,1);
+input(50001:100000,1)=1;
 
 outScanData = input; %min(max(5*randn(1e5,1),-lim),lim); %creation of signal
 inScanData = readwrite(dq,outScanData); % writes outScanData to the daq interface output channels, and reads inScanData from the daq interface input channels
@@ -23,5 +23,5 @@ hold off
 
 
 H=system_identification_fc(outScanData, inScanData.Dev1_ai1,K);
-%csvwrite('H_noise',H);
+%save('H_noise_not_flipped','H');
 
